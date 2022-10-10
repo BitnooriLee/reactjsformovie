a6d9ce4f1a2f77b47076105e70c3c581bdd71ffe
bitnoori <bitnoori.lee@gmail.com>
2022-10-10 10:21:12 +0200

    Fix Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import "./styles.css";
 
 
@@ -10,9 +10,9 @@ import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter basename={process.env.PUBLIC_URL}>
+<HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-</BrowserRouter>
+</HashRouter>
 );
 
 
