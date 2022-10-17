12438b4e27fd51937be3de0c643eb60c001dbe5f
bitnoori <bitnoori.lee@gmail.com>
2022-10-17 23:05:27 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactjsformovie/precache-manifest.23913a26d60910156b445fd5de8f85ee.js"
+  "/reactjsformovie/precache-manifest.2474c8e0d24515ad6809ef1dfa7c3afa.js"
 );
 
 workbox.clientsClaim();
